a0053b029c28bbf74cc92f9b9abeeae777c5b987
Rémy Perona <remperona@gmail.com>
2026-07-09 08:21:01 -0400

    Initial commit

---
+++ Tests/Integration/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+declare( strict_types=1 );
+
+namespace WPMedia\PackageTemplate\Tests\Integration;
+
+define( 'WPMEDIA_PACKAGE_TEMPLATE_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WPMEDIA_PACKAGE_TEMPLATE_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WPMEDIA_PACKAGE_TEMPLATE_TESTS_DIR', __DIR__ );
+
+tests_add_filter(
+	'muplugins_loaded',
+	function () {
+		require_once WPMEDIA_PACKAGE_TEMPLATE_PLUGIN_ROOT . 'plugin.php';
+	}
+);
---
+++ Tests/Unit/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace WPMedia\PackageTemplate\Tests\Unit;
+
+define( 'WPMEDIA_PACKAGE_TEMPLATE_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WPMEDIA_PACKAGE_TEMPLATE_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WPMEDIA_PACKAGE_TEMPLATE_TESTS_DIR', __DIR__ );
