1b520091eb1a9dc914495b36b34c60f816da5faa
Rémy Perona <remperona@gmail.com>
2026-07-10 12:32:00 -0400

    update base configs for unit/integration tests

--- Tests/Integration/bootstrap.php
+++ Tests/Integration/bootstrap.php
@@ -1,15 +1,16 @@
 <?php
 declare( strict_types=1 );
 
-namespace WPMedia\PackageTemplate\Tests\Integration;
+namespace WPMedia\MCP\OAuth\Tests\Integration;
 
-define( 'WPMEDIA_PACKAGE_TEMPLATE_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
-define( 'WPMEDIA_PACKAGE_TEMPLATE_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
-define( 'WPMEDIA_PACKAGE_TEMPLATE_TESTS_DIR', __DIR__ );
+define( 'WPMEDIA_MCP_OAUTH_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WPMEDIA_MCP_OAUTH_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WPMEDIA_MCP_OAUTH_TESTS_DIR', __DIR__ );
 
 tests_add_filter(
 	'muplugins_loaded',
 	function () {
-		require_once WPMEDIA_PACKAGE_TEMPLATE_PLUGIN_ROOT . 'plugin.php';
+		require_once WPMEDIA_MCP_OAUTH_PLUGIN_ROOT . 'vendor/autoload.php';
+		\WPMedia\MCP\OAuth\Bootstrap::instance();
 	}
 );
--- Tests/Unit/bootstrap.php
+++ Tests/Unit/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace WPMedia\PackageTemplate\Tests\Unit;
+namespace WPMedia\MCP\OAuth\Tests\Unit;
 
-define( 'WPMEDIA_PACKAGE_TEMPLATE_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
-define( 'WPMEDIA_PACKAGE_TEMPLATE_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
-define( 'WPMEDIA_PACKAGE_TEMPLATE_TESTS_DIR', __DIR__ );
+define( 'WPMEDIA_MCP_OAUTH_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WPMEDIA_MCP_OAUTH_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WPMEDIA_MCP_OAUTH_TESTS_DIR', __DIR__ );
