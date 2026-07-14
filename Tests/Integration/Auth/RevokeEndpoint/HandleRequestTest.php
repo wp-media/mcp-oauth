c19b62d7142d6f3a0bbb0bf642a0394e34e9c007
Rémy Perona <remperona@gmail.com>
2026-07-14 11:50:24 -0400

    move file

--- Tests/Integration/Auth/RevokeEndpoint/HandleRequestTest.php
+++ Tests/Integration/Auth/RevokeEndpoint/HandleRequestTest.php
@@ -1,7 +1,7 @@
 <?php
 declare( strict_types=1 );
 
-namespace WPMedia\MCP\OAuth\Tests\Integration\Auth;
+namespace WPMedia\MCP\OAuth\Tests\Integration\Auth\RevokeEndpoint;
 
 use WPMedia\MCP\OAuth\Auth\JWT;
 use WPMedia\MCP\OAuth\Auth\RevokeEndpoint;
